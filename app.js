9088525d268a2d79bd377dda3ba5799d106d458b
Stephen McLean <150471173+SM-SMcLean@users.noreply.github.com>
2024-12-28 19:35:15 +0000

    Add html file and update app.js
    added html file and updated app.js with basic routing

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const http = require('http'); const PORT = 3000;
+const fs = require('fs');
+const htmlfile = fs.readFileSync(__dirname + '/html/hellonode.html');
+
+
 const server = http.createServer((req, res) => {
-    console.log('request starting . . .');
-    res.setHeader("Content-Type", "text/html");
-    res.write('Hello from Node Web Server!');
-    res.end();
+    const currentpath = req.url;
+      if (currentpath === '/') {
+          res.writeHead(200, { 'Content-Type': 'text/html' });
+          res.write(htmlfile);
+          res.end();
+      } else {
+          res.writeHead(404, { 'Content-Type': 'text/html' });
+          res.write('<h1>Page not found!</h1>');
+          res.end();
+}
 });
 server.listen(PORT, (err) => {
     if (err)
