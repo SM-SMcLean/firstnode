ee520e4af025f1710178d1ab0d7167f00ea47343
Stephen McLean <150471173+SM-SMcLean@users.noreply.github.com>
2024-12-28 19:11:21 +0000

    Update app.js
    update app.js file with response

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const http = require('http'); const PORT = 3000;
-const server = http.createServer((req, res) => { console.log('request starting . . .');
+const server = http.createServer((req, res) => {
+    console.log('request starting . . .');
+    res.setHeader("Content-Type", "text/html");
+    res.write('Hello from Node Web Server!');
+    res.end();
 });
-server.listen(PORT, (err) => { if (err)
-return console.log(err);
-console.log(`Node Web Server listening on port http://localhost:${PORT}`); });
+server.listen(PORT, (err) => {
+    if (err)
+        return console.log(err);
+    console.log(`Node Web Server listening on port http://localhost:${PORT}`);
+});
